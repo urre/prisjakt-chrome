35f3e6c23e3c98dea31f7039099b94305a95f430
Urban Sanden <hej@urre.me>
2016-03-20 20:09:40 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Prisjakt högerklick
+
+![favicon](https://cloud.githubusercontent.com/assets/307676/13906485/66a6568a-eed7-11e5-92b6-7f5aa41f8229.png)
+
+Chrome Extension for searching selected text in swedish price comparison service [Prisjakt.nu](http://prisjakt.nu).
+
+## Install
+
++ In Chrome open extensions. Window -> Extensions. Or goto this url in the Chrome address bar ``open chrome://extensions/``
++ Click + Developer mode ("Programmerarläge")
++ Click Load unpacked extension… ("Hämta okomprimerat tillägg")
++ Navigate to the ``prisjakt-hogerklick`` folder, where you saved it
++ Done!
+
+## Usage
+
+1. Select text on a webpage
+2. Right click
+3. Choose "Prisjakt"
+
+## Changelog
+
+#### 2016-03-20
+First version released
---
+++ js/background.js
@@ -0,0 +1,45 @@
+/*-------------------------------------------------------------------
+Prisjakt Högerklick
+@urre 160320
+-------------------------------------------------------------------*/
+
+(function() {
+
+	'use strict';
+
+	var prisjaktHogerklick = {
+
+		init: function() {
+
+			chrome.contextMenus.onClicked.addListener(prisjaktHogerklick.selectText);
+
+			chrome.runtime.onInstalled.addListener(function() {
+				var context = "selection";
+				var title = "Prisjakt";
+				var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+					"id": "context" + context});  
+			});
+
+		},
+
+		selectText: function(info, tab) {
+			var phrase = info.selectionText;
+			prisjaktHogerklick.openPrisjakt(phrase);
+		},
+
+		openPrisjakt: function(phrase) {
+			var encodedQuery = encodeURIComponent("define " + phrase);
+			var url = "http://www.prisjakt.nu/#rparams=ss=" + encodedQuery;
+			window.open(url, '_blank');
+		}
+
+	};
+
+	document.addEventListener('DOMContentLoaded', function() { 
+	        prisjaktHogerklick.init();
+	    });
+
+
+
+}());
+
